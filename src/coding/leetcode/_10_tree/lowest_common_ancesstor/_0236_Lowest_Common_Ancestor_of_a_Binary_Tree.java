fa41055f91b7271834ece9bf55423140397b6a59
heyx8826 <155028525@qq.com>
2021-09-19 14:32:24 -0700

    _0236_Lowest_Common_Ancestor_of_a_Binary_Tree

--- src/coding/leetcode/_10_tree/lowest_common_ancesstor/_0236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
+++ src/coding/leetcode/_10_tree/lowest_common_ancesstor/_0236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -1,8 +1,3 @@
-/**
- * @author Yunxiang He
- * @date 01/28/2018
- */
-
 package coding.leetcode._10_tree.lowest_common_ancesstor;
 
 import common.TreeNode;
@@ -11,15 +6,8 @@ import common.TreeNode;
 
 Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
-
-Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
-        _______3______
-       /              \
-    ___5__          ___1__
-   /      \        /      \
-   6      _2       0       8
-         /  \
-         7   4
+According to the definition of LCA on Wikipedia:
+“The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
 
 Example 1:
@@ -28,7 +16,7 @@ Example 1:
     Output:
         3
     Explanation:
-        The LCA of of nodes 5 and 1 is 3.
+        The LCA of nodes 5 and 1 is 3.
 
 Example 2:
     Input:
@@ -38,15 +26,21 @@ Example 2:
     Explanation:
         The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
 
+Example 3:
+    Input: root = [1,2], p = 1, q = 2
+    Output: 1
 
-Note:
-    All of the nodes' values will be unique.
-    p and q are different and both values will exist in the binary tree.
+
+Constraints:
+    The number of nodes in the tree is in the range [2, 105].
+    -109 <= Node.val <= 109
+    All Node.val are unique.
+    p != q
+    p and q will exist in the tree.
 
 */
 
 public class _0236_Lowest_Common_Ancestor_of_a_Binary_Tree {
-
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
