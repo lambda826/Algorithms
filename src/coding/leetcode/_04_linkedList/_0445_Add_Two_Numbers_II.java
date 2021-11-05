58cd9f1849b1041b59843e5126b0d688a6966b1d
heyx8826 <155028525@qq.com>
2021-11-05 01:01:35 -0700

    _0445_Add_Two_Numbers_II

--- src/coding/leetcode/_04_linkedList/_0445_Add_Two_Numbers_II.java
+++ src/coding/leetcode/_04_linkedList/_0445_Add_Two_Numbers_II.java
@@ -1,9 +1,4 @@
-/**
- *  @author Yunxiang He
- *  @date 12/23/2017
- */
-
-package coding.leetcode.temp;
+package coding.leetcode._04_linkedList;
 
 import common.ListNode;
 
@@ -12,45 +7,43 @@ import java.util.Deque;
 
 /*
 
-You are given two non-empty linked lists representing two non-negative integers. 
-The most significant digit comes first and each of their nodes contain a single digit. 
-Add the two numbers and return it as a linked list.
+You are given two non-empty linked lists representing two non-negative integers.
+The most significant digit comes first and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
 
-Example:
-    Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
-    Output: 7 -> 8 -> 0 -> 7
+Example 1:
+    Input:
+        l1 = [7,2,4,3],
+        l2 = [5,6,4]
+    Output:
+        [7,8,0,7]
+
+Example 2:
+    Input:
+        l1 = [2,4,3],
+        l2 = [5,6,4]
+    Output:
+        [8,0,7]
+
+Example 3:
+    Input:
+        l1 = [0],
+        l2 = [0]
+    Output:
+        [0]
 
 
-Follow up:
-    What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+Constraints:
+    The number of nodes in each linked list is in the range [1, 100].
+    0 <= Node.val <= 9
+    It is guaranteed that the list represents a number that does not have leading zeros.
 
 */
 
 public class _0445_Add_Two_Numbers_II {
-
-    public static void main(String[] args) {
-        ListNode l1 = new ListNode(7);
-        ListNode l2 = new ListNode(2);
-        ListNode l3 = new ListNode(4);
-        ListNode l4 = new ListNode(3);
-        l1.next = l2;
-        l2.next = l3;
-        l3.next = l4;
-
-        ListNode l5 = new ListNode(5);
-        ListNode l6 = new ListNode(6);
-        ListNode l7 = new ListNode(4);
-        l5.next = l6;
-        l6.next = l7;
-
-        ListNode res = new _0445_Add_Two_Numbers_II().addTwoNumbers2(l1, l5);
-        while (res != null) {
-            System.out.print(res.val);
-            res = res.next;
-        }
-    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -125,55 +118,57 @@ public class _0445_Add_Two_Numbers_II {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // 1. padding 0 to the front of the shorter linkedlist
     // 2. recursively add the two lists
-    public ListNode addTwoNumbers3(ListNode l1, ListNode l2) {
-        // 1
-        ListNode _l1 = l1;
-        ListNode _l2 = l2;
-        int c1 = 0;
-        while (_l1 != null) {
-            c1++;
-            _l1 = _l1.next;
-        }
-        int c2 = 0;
-        while (_l2 != null) {
-            c2++;
-            _l2 = _l2.next;
-        }
-        int d = c1 - c2;
-        ListNode temp;
-        if (d < 0) {
-            while (d != 0) {
-                temp = new ListNode(0);
-                temp.next = l1;
-                l1 = temp;
-                d++;
+    class Solution {
+
+        private int carry = 0;
+
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            int count1 = 0;
+            int count2 = 0;
+            ListNode ll1 = l1;
+            ListNode ll2 = l2;
+            while (ll1 != null) {
+                ll1 = ll1.next;
+                ++count1;
             }
-        } else {
-            while (d != 0) {
-                temp = new ListNode(0);
-                temp.next = l2;
-                l2 = temp;
-                d--;
+            while (ll2 != null) {
+                ll2 = ll2.next;
+                ++count2;
             }
+            if (count1 > count2) {
+                l2 = padding(l2, count1 - count2);
+            } else {
+                l1 = padding(l1, count2 - count1);
+            }
+            ListNode res = add(l1, l2);
+            if (carry != 0) {
+                ListNode listNode = new ListNode(1);
+                listNode.next = res;
+                res = listNode;
+            }
+            return res;
         }
-        // 2
-        ListNode l = new ListNode(1);
-        ListNode next = add(l1, l2);
-        l.next = next;
-        return carry == 1 ? l : next;
-    }
 
-    private int carry = 0;
-
-    private ListNode add(ListNode l1, ListNode l2) {
-        ListNode node = new ListNode(0);
-        if (l1.next != null) {
-            node.next = add(l1.next, l2.next);
+        private ListNode add(ListNode l1, ListNode l2) {
+            ListNode node = new ListNode();
+            if (l1.next != null) {
+                node.next = add(l1.next, l2.next);
+            }
+            carry += l1.val + l2.val;
+            node.val = carry % 10;
+            carry /= 10;
+            return node;
         }
-        carry += l1.val + l2.val;
-        node.val = carry % 10;
-        carry /= 10;
-        return node;
-    }
 
+        private ListNode padding(ListNode l, int count) {
+            ListNode head = new ListNode(0);
+            head.next = l;
+            while (count-- > 0) {
+                ListNode node = new ListNode(0);
+                node.next = head.next;
+                head.next = node;
+            }
+            return head.next;
+        }
+    }
 }
