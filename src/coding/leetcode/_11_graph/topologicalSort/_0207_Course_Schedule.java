fccc66203be804f813d1a94d59fd89a2263292bd
heyx8826 <155028525@qq.com>
2021-11-10 15:47:36 -0800

    _0207_Course_Schedule

--- src/coding/leetcode/_11_graph/topologicalSort/_0207_Course_Schedule.java
+++ src/coding/leetcode/_11_graph/topologicalSort/_0207_Course_Schedule.java
@@ -8,7 +8,7 @@ import java.util.Queue;
 /*
 
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
-You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+You are given an array prerequisites where prerequisites[i] = [a(i), b(i)] indicates that you must take course bi first if you want to take course a(i).
     For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
 Return true if you can finish all courses. Otherwise, return false.
 
@@ -36,10 +36,10 @@ Example 2:
 
 
 Constraints:
-    1 <= numCourses <= 10^40
+    1 <= numCourses <= 10^5
     0 <= prerequisites.length <= 5000
     prerequisites[i].length == 2
-    0 <= ai, bi < numCourses
+    0 <= a(i), b(i) < numCourses
     All the pairs prerequisites[i] are unique.
 
 */
@@ -61,46 +61,47 @@ public class _0207_Course_Schedule {
     //          visited[i] == 0 means node i is unvisited;
     //          visited[i] == 1 means node i is in frontier;
     //          visited[i] == 2 means node i is visited;
-    public boolean canFinish_DFS(int numCourses, int[][] prerequisites) {
-        List<Integer>[] adjacentList = buildGraph(numCourses, prerequisites);
-        int[] visited = new int[numCourses];
-        for (int i = 0; i < numCourses; ++i) {
-            if (visited[i] == 0) {
-                if (containsCycle(adjacentList, i, visited)) {
+
+    class Solution_DFS {
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            List<Integer>[] adjacentList = buildGraph(numCourses, prerequisites);
+            int[] visited = new int[numCourses];
+            for (int i = 0; i < numCourses; ++i) {
+                if (visited[i] == 0 && containsCycleDFS(adjacentList, i, visited)) {
                     return false;
                 }
             }
+            return true;
         }
-        return true;
-    }
 
-    private boolean containsCycle(List<Integer>[] adjacentList, int current, int[] visited) {
-        if (visited[current] == 0) {
-            visited[current] = 1; // Current node is in frontier
-            if (adjacentList[current] != null) {
-                for (int next : adjacentList[current]) {
-                    // If the next node is already in the frontier
-                    if (visited[next] == 1 || containsCycle(adjacentList, next, visited)) {
-                        return true;
+        private boolean containsCycleDFS(List<Integer>[] adjacentList, int current, int[] visited) {
+            if (visited[current] == 0) {
+                visited[current] = 1; // Current node is in frontier
+                if (adjacentList[current] != null) {
+                    for (int next : adjacentList[current]) {
+                        // If the next node is already in the frontier
+                        if (visited[next] == 1 || containsCycleDFS(adjacentList, next, visited)) {
+                            return true;
+                        }
                     }
                 }
+                visited[current] = 2; // Current node is visited
             }
-            visited[current] = 2; // Current node is visited
+            return false;
         }
-        return false;
-    }
 
-    private List<Integer>[] buildGraph(int numCourses, int[][] prerequisites) {
-        // Build graph as Adjacent List representation given edges
-        List<Integer>[] adjacentList = new List[numCourses];
-        for (int[] prerequisite : prerequisites) {
-            // Initialize an ArrayList if it's first time visit the node
-            if (adjacentList[prerequisite[1]] == null) {
-                adjacentList[prerequisite[1]] = new ArrayList<>();
+        private List<Integer>[] buildGraph(int numCourses, int[][] prerequisites) {
+            // Build graph as Adjacent List representation given edges
+            List<Integer>[] adjacentList = new List[numCourses];
+            for (int[] prerequisite : prerequisites) {
+                // Initialize an ArrayList if it's first time visit the node
+                if (adjacentList[prerequisite[1]] == null) {
+                    adjacentList[prerequisite[1]] = new ArrayList<>();
+                }
+                adjacentList[prerequisite[1]].add(prerequisite[0]); // Add the neighbour into neighbour list
             }
-            adjacentList[prerequisite[1]].add(prerequisite[0]); // Add the neighbour into neighbour list
+            return adjacentList;
         }
-        return adjacentList;
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -113,42 +114,43 @@ public class _0207_Course_Schedule {
     //          2.2 Update In-Degrees of each neighbour of current visiting node;
     //          2.3 Enqueue the neighbour if the updated In-Degree becomes zero;
     //      3. Check the size of the order list;
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // Build Graph using Adjacent List with given edges
-        List<Integer>[] adjacentList = new List[numCourses];
-        int[] inDegrees = new int[numCourses];
-        for (int[] prerequisite : prerequisites) {
-            if (adjacentList[prerequisite[1]] == null) {
-                adjacentList[prerequisite[1]] = new ArrayList<>();
+    class Solution_BFS {
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            // Build Graph using Adjacent List with given edges
+            List<Integer>[] adjacentList = new List[numCourses];
+            int[] inDegrees = new int[numCourses];
+            for (int[] prerequisite : prerequisites) {
+                if (adjacentList[prerequisite[1]] == null) {
+                    adjacentList[prerequisite[1]] = new ArrayList<>();
+                }
+                adjacentList[prerequisite[1]].add(prerequisite[0]);
+                inDegrees[prerequisite[0]]++; // Build inDegree array
             }
-            adjacentList[prerequisite[1]].add(prerequisite[0]);
-            inDegrees[prerequisite[0]]++; // Build inDegree array
-        }
 
-        // Enqueue nodes with zero inDegree
-        Queue<Integer> zeroInDegreeQueue = new ArrayDeque<>();
-        for (int i = 0; i < inDegrees.length; ++i) {
-            if (inDegrees[i] == 0) {
-                zeroInDegreeQueue.offer(i);
+            // Enqueue nodes with zero inDegree
+            Queue<Integer> zeroInDegreeQueue = new ArrayDeque<>();
+            for (int i = 0; i < inDegrees.length; ++i) {
+                if (inDegrees[i] == 0) {
+                    zeroInDegreeQueue.offer(i);
+                }
             }
-        }
 
-        // BFS
-        List<Integer> order = new ArrayList<>();
-        while (!zeroInDegreeQueue.isEmpty()) {
-            int curr = zeroInDegreeQueue.poll();
-            order.add(curr);
-            if (adjacentList[curr] != null) {
-                for (int next : adjacentList[curr]) {
-                    // -1 inDegree for each neighbour of current node
-                    // Enqueue if inDegree of the neighbour is zero
-                    if (--inDegrees[next] == 0) {
-                        zeroInDegreeQueue.offer(next);
+            // BFS
+            List<Integer> order = new ArrayList<>();
+            while (!zeroInDegreeQueue.isEmpty()) {
+                int curr = zeroInDegreeQueue.poll();
+                order.add(curr);
+                if (adjacentList[curr] != null) {
+                    for (int next : adjacentList[curr]) {
+                        // -1 inDegree for each neighbour of current node
+                        // Enqueue if inDegree of the neighbour is zero
+                        if (--inDegrees[next] == 0) {
+                            zeroInDegreeQueue.offer(next);
+                        }
                     }
                 }
             }
+            return order.size() == numCourses;
         }
-        return order.size() == numCourses;
     }
-
 }
