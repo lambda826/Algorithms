52b115e85f18a6c94883ed49acfe988ecaa33e3b
heyx8826 <155028525@qq.com>
2021-11-10 18:31:59 -0800

    _0133_Clone_Graph

--- src/coding/leetcode/_11_graph/_0133_Clone_Graph.java
+++ src/coding/leetcode/_11_graph/_0133_Clone_Graph.java
@@ -11,37 +11,68 @@ import java.util.Queue;
 
 /*
 
-Given the head of a graph, return a deep copy (clone) of the graph. 
-Each node in the graph contains a label (int) and a list (List[GraphNode]) of its neighbors. 
-There is an edge between the given node and each of the nodes in its neighbors.
+Given a reference of a node in a connected undirected graph.
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+    }
 
 
-OJ's undirected graph serialization (so you can understand error output):
-Nodes are labeled uniquely.
-We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
- 
-
-As an example, consider the serialized graph {0,1,2#1,2#2,2}.
-
-The graph has a total of three nodes, and therefore contains three parts as separated by #.
-    First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
-    Second node is labeled as 1. Connect node 1 to node 2.
-    Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
- 
-
-Visually, the graph looks like the following:
-
-       1
-      / \
-     /   \
-    0 --- 2
-         / \
-         \_/
+Test case format:
+    For simplicity, each node's value is the same as the node's index (1-indexed).
+        For example, the first node with val == 1, the second node with val == 2, and so on.
+        The graph is represented in the test case using an adjacency list.
+    An adjacency list is a collection of unordered lists used to represent a finite graph.
+    Each list describes the set of neighbors of a node in the graph.
+    The given node will always be the first node with val = 1.
+    You must return the copy of the given node as a reference to the cloned graph.
 
 
-Note: 
-    The information about the tree serialization is only meant so that you can understand error output if you get a wrong answer. 
-    You don't need to understand the serialization to solve the problem.
+Example 1:
+    Input:
+        adjList = [[2,4],[1,3],[2,4],[1,3]]
+    Output:
+        [[2,4],[1,3],[2,4],[1,3]]
+    Explanation:
+        There are 4 nodes in the graph.
+            1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+            2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+            3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+            4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+
+Example 2:
+    Input:
+        adjList = [[]]
+    Output:
+        [[]]
+    Explanation:
+        Note that the input contains one empty list.
+        The graph consists of only one node with val = 1 and it does not have any neighbors.
+
+Example 3:
+    Input:
+        adjList = []
+    Output:
+        []
+    Explanation:
+        This an empty graph, it does not have any nodes.
+
+Example 4:
+    Input:
+        adjList = [[2],[1]]
+    Output:
+        [[2],[1]]
+
+
+Constraints:
+    The number of nodes in the graph is in the range [0, 100].
+    1 <= Node.val <= 100
+    Node.val is unique for each node.
+    There are no repeated edges and no self-loops in the graph.
+    The Graph is connected and all nodes can be visited starting from the given node.
 
 */
 
@@ -89,7 +120,6 @@ public class _0133_Clone_Graph {
             }
             return visited.get(node);
         }
-
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
