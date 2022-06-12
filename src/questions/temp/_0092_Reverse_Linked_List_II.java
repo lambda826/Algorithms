85d733984a89ac5aeb786d53c60dff522c59a7a9
heyx8826 <155028525@qq.com>
2022-06-12 00:01:21 -0700

    _0092_Reverse_Linked_List_II

--- src/questions/temp/_0092_Reverse_Linked_List_II.java
+++ src/questions/temp/_0092_Reverse_Linked_List_II.java
@@ -1,55 +1,65 @@
-/**
- *  @author Yunxiang He
- *  @date 03/17/2019
- */
-
 package questions.temp;
 
 import common.ListNode;
 
 /*
 
-Reverse a linked list from position m to n. 
-Do it in one-pass.
+Given the head of a singly linked list and two integers left and right where left <= right,
+reverse the nodes of the list from position left to position right, and return the reversed list.
 
 
-Example:
-    Input: 1->2->3->4->5->NULL, m = 2, n = 4
-    Output: 1->4->3->2->5->NULL
+Example 1:
+    Input:
+        head = [1,2,3,4,5],
+        left = 2,
+        right = 4
+    Output:
+        [1,4,3,2,5]
 
-Note: 
-    1 ≤ m ≤ n ≤ length of list.
+Example 2:
+    Input:
+        head = [5],
+        left = 1,
+        right = 1
+    Output: [5]
+
+
+Constraints:
+    The number of nodes in the list is n.
+    1 <= n <= 500
+    -500 <= Node.val <= 500
+    1 <= left <= right <= n
 
 */
-
 public class _0092_Reverse_Linked_List_II {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // find the start point
-    // reverse until the end point
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        head = dummy;
-        while (m > 1) {
-            head = head.next;
-            --n;
-            --m;
+    // Find correct pre and curr position;
+    // Insert curr.next into pre.next.
+    class Solution {
+        public ListNode reverseBetween(ListNode head, int left, int right) {
+            if (left == right) {
+                return head;
+            }
+            ListNode res = new ListNode();
+            res.next = head;
+            ListNode pre = res;
+            ListNode curr = head;
+            while (left > 1 && right > 1) {
+                left--;
+                right--;
+                curr = curr.next;
+                pre = pre.next;
+            }
+            while (right - 1 > 0) {
+                right--;
+                ListNode next = curr.next;
+                curr.next = next.next;
+                next.next = pre.next;
+                pre.next = next;
+            }
+            return res.next;
         }
-        ListNode h = head.next;
-        ListNode curr = h;
-        ListNode pre = null;
-        ListNode next = null;
-        // walk n steps, pre is the reversed list, curr is the next node
-        while (n-- > 0) {
-            next = curr.next;
-            curr.next = pre;
-            pre = curr;
-            curr = next;
-        }
-        head.next = pre;
-        h.next = curr;
-        return dummy.next;
     }
 }
