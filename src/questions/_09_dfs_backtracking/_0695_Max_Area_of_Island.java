53201bd7f8bcb62ee50aba256e2a26d00cca0dba
heyx8826 <155028525@qq.com>
2022-07-24 16:05:54 -0700

    _0695_Max_Area_of_Island

--- src/questions/_09_dfs_backtracking/_0695_Max_Area_of_Island.java
+++ src/questions/_09_dfs_backtracking/_0695_Max_Area_of_Island.java
@@ -42,34 +42,129 @@ Constraints:
 
 */
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
 public class _0695_Max_Area_of_Island {
 
     private static final int[] dir = { -1, 0, 1, 0, -1 };
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    class Solution_DFS {
+    class Solution {
+
+        private final int[] dir = { -1, 0, 1, 0, -1 };
 
         public int maxAreaOfIsland(int[][] grid) {
             int max = 0;
             for (int i = 0; i < grid.length; ++i) {
                 for (int j = 0; j < grid[0].length; ++j) {
-                    max = Math.max(max, DFS(grid, i, j));
+                    max = Math.max(max, getArea(i, j, grid));
                 }
             }
             return max;
         }
 
-        private int DFS(int[][] grid, int i, int j) {
-            int sum = 0;
+        private int getArea(int i, int j, int[][] grid) {
+            int area = 0;
             if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1) {
-                sum = 1;
                 grid[i][j] = 0;
-                for (int d = 0; d < 4; ++d) {
-                    sum += DFS(grid, i + dir[d], j + dir[d + 1]);
+                area = 1;
+                for (int k = 0; k < 4; ++k) {
+                    area += getArea(i + dir[k], j + dir[k + 1], grid);
                 }
             }
-            return sum;
+            return area;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Followup: 是要把 enclosed water cell 也算进去
+    // Clarification: a water cell can be enclosed by multiple islands, how should we count this case?
+    //                  are those islands considered connected or separate?
+    static class Solution_FollowUp {
+
+        private final int[] dir = { -1, 0, 1, 0, -1 };
+
+        public int maxAreaOfIsland(int[][] grid) {
+
+            // Find all water cells which are not enclosed
+            for (int i = 0; i < grid.length; ++i) {
+                for (int j = 0; j < grid[0].length; ++j) {
+                    if (i == 0 || i == grid.length - 1 || j == 0 || j == grid[0].length) {
+                        getArea(i, j, grid, -1, n -> n == 0);
+                    }
+                }
+            }
+
+            // Mark different island
+            Map<Integer, Integer> islandArea = new HashMap<>();
+            int mark = 1;
+            for (int i = 0; i < grid.length; ++i) {
+                for (int j = 0; j < grid[0].length; ++j) {
+                    if (grid[i][j] == 1) {
+                        ++mark;
+                        islandArea.put(mark, getArea(i, j, grid, mark, n -> n == 1));
+                    }
+                }
+            }
+
+            // Update enclose water
+            for (int i = 0; i < grid.length; ++i) {
+                for (int j = 0; j < grid[0].length; ++j) {
+                    if (grid[i][j] == 0) {
+                        Integer[] enclose = new Integer[] { null };
+                        islandArea.put(enclose[0], islandArea.getOrDefault(enclose[0], 0)
+                                                   + updateEncloseWater(i, j, grid, enclose));
+                    }
+                }
+            }
+            islandArea.remove(0);
+            return islandArea.values().stream().mapToInt(Integer::intValue).max().getAsInt();
+        }
+
+        private int getArea(int i, int j, int[][] grid, int update, Predicate<Integer> isTarget) {
+            int area = 0;
+            if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && isTarget.test(grid[i][j])) {
+                grid[i][j] = update;
+                area = 1;
+                for (int k = 0; k < 4; ++k) {
+                    area += getArea(i + dir[k], j + dir[k + 1], grid, update, isTarget);
+                }
+            }
+            return area;
+        }
+
+        private int updateEncloseWater(int i, int j, int[][] grid, Integer[] enclose) {
+            int area = 0;
+            if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length) {
+                if (grid[i][j] == 0) {
+                    area = 1;
+                    grid[i][j] = -1;
+                    for (int k = 0; k < 4; ++k) {
+                        area += updateEncloseWater(i + dir[k], j + dir[k + 1], grid, enclose);
+                    }
+                } else if (enclose[0] == null) {
+                    enclose[0] = grid[i][j];
+                } else if (enclose[0] != grid[i][j]) {
+                    enclose[0] = 0;
+                }
+            }
+            return area;
+        }
+
+        public static void main(String[] args) {
+            System.out.println(new Solution_FollowUp().maxAreaOfIsland(
+                    new int[][] { { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                            { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                            { 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0 },
+                            { 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },
+                            { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },
+                            { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0 },
+                            { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                            { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 } }));
         }
     }
 }
