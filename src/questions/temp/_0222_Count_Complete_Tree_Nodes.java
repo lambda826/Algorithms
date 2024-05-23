92ef22bf0c7abf94ab68eed932369f3e17dfe7fe
heyx8826 <155028525@qq.com>
2024-05-22 22:16:39 -0700

    _0222_Count_Complete_Tree_Nodes
    Signed-off-by: heyx8826 <155028525@qq.com>

--- src/questions/temp/_0222_Count_Complete_Tree_Nodes.java
+++ src/questions/temp/_0222_Count_Complete_Tree_Nodes.java
@@ -1,115 +1,104 @@
-/**
- *  @author Yunxiang He
- *  @date 05/05/2018
- */
-
 package questions.temp;
 
 import common.TreeNode;
 
 /*
 
-Given a complete binary tree, count the number of nodes.
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible.
+It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
 
 
-Example:
-    Input: 
-        1
-       / \
-      2   3
-     / \  /
-    4  5 6
+Example 1:
+    Input: root = [1,2,3,4,5,6]
     Output: 6
+Example 2:
+    Input: root = []
+    Output: 0
+Example 3:
+    Input: root = [1]
+    Output: 1
 
- */
 
+Constraints:
+    The number of nodes in the tree is in the range [0, 5 * 104].
+    0 <= Node.val <= 5 * 104
+    The tree is guaranteed to be complete.
+
+*/
 public class _0222_Count_Complete_Tree_Nodes {
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Find whether the tree has the mid node of the last layer
-    public int countNodes(TreeNode root) {
-        TreeNode tmp = root;
-        int ht = 0;
-        while (tmp != null) {
-            ++ht;
-            tmp = tmp.left;
+    class Solution {
+        public int countNodes(TreeNode root) {
+            int height = 0;
+            TreeNode node = root;
+            while (node != null) {
+                ++height;
+                node = node.left;
+            }
+            int[] num = { 0 };
+            dfs(root, height, num, new boolean[] { false });
+            return (1 << height) - 1 - num[0];
         }
-        return count(root, ht - 1);
-    }
 
-    private int count(TreeNode node, int ht) {
-        if (node == null) {
-            return 0;
-        } else if (node.left == null) {
-            return 1;
-        } else if (hasMid(node.left, ht)) {
-            return (1 << ht) + count(node.right, ht - 1);
-        } else {
-            return (1 << (ht - 1)) + count(node.left, ht - 1);
-        }
-    }
-
-    private boolean hasMid(TreeNode node, int ht) {
-        while (node != null) {
-            node = node.right;
-            --ht;
-        }
-        return ht == 0;
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Binary search
-    // Find the height of the tree
-    // The node is between [2 ^ (h - 1), 2 ^ h - 1]
-    public int countNodes2(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int height = 0;
-        TreeNode tmp = root;
-        while (tmp != null) {
-            ++height;
-            tmp = tmp.left;
-        }
-        int lo = 1 << (height - 1);
-        int hi = (1 << height) - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (isExist(mid, root)) {
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
+        private void dfs(TreeNode node, int height, int[] num, boolean[] isStop) {
+            if (!isStop[0]) {
+                if (node == null) {
+                    if (height == 0) {
+                        isStop[0] = true;
+                    } else {
+                        num[0]++;
+                    }
+                } else {
+                    dfs(node.right, height - 1, num, isStop);
+                    dfs(node.left, height - 1, num, isStop);
+                }
             }
         }
-        return hi;
     }
 
-    private boolean isExist(int mid, TreeNode root) {
-        String digits = Integer.toBinaryString(mid);
-        for (int i = 1; i < digits.length(); i++) {
-            if (digits.charAt(i) == '0') {
-                root = root.left;
-            } else {
-                root = root.right;
+    class Solution2 {
+
+        public int countNodes(TreeNode root) {
+            int height = 0;
+            TreeNode node = root;
+            // Calculate the height of the tree by traversing the leftmost path.
+            while (node != null) {
+                ++height;
+                node = node.left;
             }
+            int sum = 0;
+            node = root;
+            // Traverse the tree and count nodes.
+            while (node != null) {
+                height--;
+                int h = getHeight(node.left);
+                // Check if the left subtree is a perfect binary tree.
+                // If h == height, it means the left subtree is a perfect binary tree (full).
+                // Thus, move to the right subtree (node = node.right) because all nodes in the left subtree are counted.
+                if (h == height) {
+                    node = node.right;
+                } else {
+                    node = node.left;
+                }
+                // Add the number of nodes in the perfect subtree.
+                sum += (1 << h);
+            }
+
+            return sum;
         }
-        return root != null;
-    }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // O(n)
-    public int countNodes3(TreeNode root) {
-        return dfs(root);
-    }
-
-    private int dfs(TreeNode node) {
-        if (node == null) {
-            return 0;
-        } else {
-            return 1 + dfs(node.left) + dfs(node.right);
+        private int getHeight(TreeNode node) {
+            int height = 0;
+            // Traverse the rightmost path to calculate the height.
+            while (node != null) {
+                ++height;
+                node = node.right;
+            }
+            return height;
         }
     }
 }
