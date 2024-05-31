e49aa3d975d3bc47d294b9368d5730426832a22f
heyx8826 <155028525@qq.com>
2024-05-30 23:07:38 -0700

    _0783_Minimum_Distance_Between_BST_Nodes
    Signed-off-by: heyx8826 <155028525@qq.com>

--- src/questions/leetcode/_0783_Minimum_Distance_Between_BST_Nodes.java
+++ src/questions/leetcode/_0783_Minimum_Distance_Between_BST_Nodes.java
@@ -7,49 +7,50 @@ import java.util.Deque;
 
 /*
 
-Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
 
 
 Example 1:
     Input: root = [4,2,6,1,3]
     Output: 1
+
 Example 2:
     Input: root = [1,0,48,null,null,12,49]
     Output: 1
 
 
 Constraints:
-    The number of nodes in the tree is in the range [2, 10^4].
+    The number of nodes in the tree is in the range [2, 100].
     0 <= Node.val <= 10^5
 
 */
-public class _0530_Minimum_Absolute_Difference_in_BST {
+public class _0783_Minimum_Distance_Between_BST_Nodes {
 
     class Solution {
-        public int getMinimumDifference(TreeNode root) {
+        public int minDiffInBST(TreeNode root) {
             int[] min = { Integer.MAX_VALUE };
             inorder(root, new Integer[] { null }, min);
             return min[0];
         }
 
-        private void inorder(TreeNode curr, Integer[] pre, int[] min) {
-            if (curr != null) {
-                inorder(curr.left, pre, min);
+        private void inorder(TreeNode node, Integer[] pre, int[] min) {
+            if (node != null) {
+                inorder(node.left, pre, min);
                 if (pre[0] != null) {
-                    min[0] = Math.min(min[0], curr.val - pre[0]);
+                    min[0] = Math.min(min[0], node.val - pre[0]);
                 }
-                pre[0] = curr.val;
-                inorder(curr.right, pre, min);
+                pre[0] = node.val;
+                inorder(node.right, pre, min);
             }
         }
     }
 
     class Solution2 {
-        public int getMinimumDifference(TreeNode root) {
+        public int minDiffInBST(TreeNode root) {
             TreeNode pre = null;
-            TreeNode curr = root;
             Deque<TreeNode> stack = new ArrayDeque<>();
             int min = Integer.MAX_VALUE;
+            TreeNode curr = root;
             while (!stack.isEmpty() || curr != null) {
                 if (curr != null) {
                     stack.offerLast(curr);
