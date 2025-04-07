cff7bca16e1ef41df87f5d5e11f57c60a2d228f5
heyx8826 <155028525@qq.com>
2025-04-06 23:40:09 -0700

    _0055_Jump_Game Solution_Optimal
    Signed-off-by: heyx8826 <155028525@qq.com>

--- src/questions/leetcode/greedy/_0055_Jump_Game.java
+++ src/questions/leetcode/greedy/_0055_Jump_Game.java
@@ -2,6 +2,18 @@ package questions.leetcode.greedy;
 
 public class _0055_Jump_Game {
 
+    class Solution_Optimal {
+        public boolean canJump(int[] nums) {
+            int lastIdx = nums.length - 1;
+            for (int i = lastIdx - 1; i >= 0; --i) {
+                if (nums[i] + i >= lastIdx) {
+                    lastIdx = i;
+                }
+            }
+            return lastIdx == 0;
+        }
+    }
+
     class Solution {
         public boolean canJump(int[] nums) {
             int step = 1;
