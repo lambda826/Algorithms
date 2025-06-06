4538a9001092be9f7f323ee78558a906ca24b1b9
heyx8826 <155028525@qq.com>
2025-06-06 00:52:15 -0700

    _0125_Valid_Palindrome
    Signed-off-by: heyx8826 <155028525@qq.com>

--- src/questions/_02_string/palindrome/_0125_Valid_Palindrome.java
+++ src/questions/_02_string/palindrome/_0125_Valid_Palindrome.java
@@ -2,52 +2,52 @@ package questions._02_string.palindrome;
 
 /*
 
-Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
 
 
 Example 1:
-    Input:
-        s = "A man, a plan, a canal: Panama"
-    Output:
-        true
-    Explanation:
-        "amanaplanacanalpanama" is a palindrome.
+    Input: s = "A man, a plan, a canal: Panama"
+    Output: true
+    Explanation: "amanaplanacanalpanama" is a palindrome.
 
 Example 2:
-    Input:
-        s = "race a car"
-    Output:
-        false
-    Explanation:
-        "raceacar" is not a palindrome.
+    Input: s = "race a car"
+    Output: false
+    Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+    Input: s = " "
+    Output: true
+    Explanation: s is an empty string "" after removing non-alphanumeric characters.
+    Since an empty string reads the same forward and backward, it is a palindrome.
 
 
 Constraints:
     1 <= s.length <= 2 * 10^5
     s consists only of printable ASCII characters.
-
- */
-
+*/
 public class _0125_Valid_Palindrome {
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Two pointer
     class Solution {
-
         public boolean isPalindrome(String s) {
-            int p1 = 0;
-            int p2 = s.length() - 1;
-            while (p1 < p2) {
-                while (p1 < p2 && !Character.isLetterOrDigit(s.charAt(p1))) {
-                    ++p1;
+            int left = 0;
+            int right = s.length() - 1;
+            while (left < right) {
+                while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                    left++;
                 }
-                while (p1 < p2 && !Character.isLetterOrDigit(s.charAt(p2))) {
-                    --p2;
+                while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                    right--;
                 }
-                if (Character.toLowerCase(s.charAt(p1++)) != (Character.toLowerCase(s.charAt(p2--)))) {
+
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                     return false;
                 }
+                left++;
+                right--;
             }
             return true;
         }
