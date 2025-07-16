fabc902629072907e3a39252ddce367fa23cc412
heyx8826 <155028525@qq.com>
2025-07-15 22:30:20 -0700

    _0207_Course_Schedule
    Signed-off-by: heyx8826 <155028525@qq.com>

--- src/questions/_11_graph/topologicalSort/_0207_Course_Schedule.java
+++ src/questions/_11_graph/topologicalSort/_0207_Course_Schedule.java
@@ -46,111 +46,84 @@ Constraints:
 
 public class _0207_Course_Schedule {
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Topological sorting - dfs Approach: find a cycle in a graph.
-    //      0. Build Graph using Adjacent List representation;
-    //      1. For each node, visit if it is not visited;
-    //      2. Update the visit status of the current node to be 1 (in the frontier);
-    //          2.1 Recursively visit each neighbour (for loop) of the current node;
-    //          2.2 Update the visit status of the current node to be 2 (finished visit);
-    //
-    // Takeaways:
-    //      1. Adjacent List (List<Integer>[] adjacentList = new List[numberOfNodes]) to represent a DAG;
-    //      2. Use an int[] array to indicate the status of the node:
-    //          visited[i] == 0 means node i is unvisited;
-    //          visited[i] == 1 means node i is in frontier;
-    //          visited[i] == 2 means node i is visited;
-
-    class Solution_DFS {
+    class Solution_Topological {
         public boolean canFinish(int numCourses, int[][] prerequisites) {
-            List<Integer>[] adjacentList = buildGraph(numCourses, prerequisites);
+            List<Integer>[] graph = new List[numCourses];
+            int[] indegree = new int[numCourses];
+            buildGraph(prerequisites, graph, indegree);
+            Queue<Integer> zeroIndegree = new ArrayDeque<>();
+            for (int i = 0; i < graph.length; ++i) {
+                if (indegree[i] == 0) {
+                    zeroIndegree.offer(i);
+                }
+            }
+            int n = 0;
+            while (!zeroIndegree.isEmpty()) {
+                int course = zeroIndegree.poll();
+                n++;
+                if (graph[course] != null) {
+                    for (int next : graph[course]) {
+                        indegree[next]--;
+                        if (indegree[next] == 0) {
+                            zeroIndegree.offer(next);
+                        }
+                    }
+                }
+            }
+            return n == numCourses;
+        }
+
+        private void buildGraph(int[][] prerequisites, List<Integer>[] graph, int[] indegree) {
+            for (int[] prerequisite : prerequisites) {
+                if (graph[prerequisite[1]] == null) {
+                    graph[prerequisite[1]] = new ArrayList<>();
+                }
+                graph[prerequisite[1]].add(prerequisite[0]);
+                indegree[prerequisite[0]]++;
+            }
+        }
+    }
+
+    class Solution_DFS_FindCircle {
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            List<Integer>[] graph = new List[numCourses];
+            buildGraph(prerequisites, graph);
             int[] visited = new int[numCourses];
             for (int i = 0; i < numCourses; ++i) {
-                if (visited[i] == 0 && containsCycleDFS(adjacentList, i, visited)) {
+                if (visited[i] == 0 && findCycle(graph, visited, i)) {
                     return false;
                 }
             }
             return true;
         }
 
-        private boolean containsCycleDFS(List<Integer>[] adjacentList, int current, int[] visited) {
-            if (visited[current] == 0) {
-                visited[current] = 1; // Current node is in frontier
-                if (adjacentList[current] != null) {
-                    for (int next : adjacentList[current]) {
-                        // If the next node is already in the frontier
-                        if (visited[next] == 1 || containsCycleDFS(adjacentList, next, visited)) {
-                            return true;
+        private boolean findCycle(List<Integer>[] graph, int[] visited, int n) {
+            if (visited[n] == 1) {
+                return true;
+            } else {
+                visited[n] = 1;
+                if (graph[n] != null) {
+                    for (int next : graph[n]) {
+                        if (visited[next] != 2) {
+                            if (findCycle(graph, visited, next)) {
+                                return true;
+                            }
                         }
                     }
                 }
-                visited[current] = 2; // Current node is visited
+                visited[n] = 2;
+                return false;
             }
-            return false;
         }
 
-        private List<Integer>[] buildGraph(int numCourses, int[][] prerequisites) {
-            // Build graph as Adjacent List representation given edges
-            List<Integer>[] adjacentList = new List[numCourses];
+        private void buildGraph(int[][] prerequisites, List<Integer>[] graph) {
             for (int[] prerequisite : prerequisites) {
-                // Initialize an ArrayList if it's first time visit the node
-                if (adjacentList[prerequisite[1]] == null) {
-                    adjacentList[prerequisite[1]] = new ArrayList<>();
+                if (graph[prerequisite[1]] == null) {
+                    graph[prerequisite[1]] = new ArrayList<>();
                 }
-                adjacentList[prerequisite[1]].add(prerequisite[0]); // Add the neighbour into neighbour list
+                graph[prerequisite[1]].add(prerequisite[0]);
             }
-            return adjacentList;
         }
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Topological sorting - BFS Approach: find a cycle in a graph.
-    //      0. Build Graph using Adjacent List representation and In-Degree Array for each node;
-    //      1. Enqueue all nodes with zero In-Degree value;
-    //      2. BFS
-    //          2.1 Add current visiting node into order list;
-    //          2.2 Update In-Degrees of each neighbour of current visiting node;
-    //          2.3 Enqueue the neighbour if the updated In-Degree becomes zero;
-    //      3. Check the size of the order list;
-    class Solution_BFS {
-        public boolean canFinish(int numCourses, int[][] prerequisites) {
-            // Build Graph using Adjacent List with given edges
-            List<Integer>[] adjacentList = new List[numCourses];
-            int[] inDegrees = new int[numCourses];
-            for (int[] prerequisite : prerequisites) {
-                if (adjacentList[prerequisite[1]] == null) {
-                    adjacentList[prerequisite[1]] = new ArrayList<>();
-                }
-                adjacentList[prerequisite[1]].add(prerequisite[0]);
-                inDegrees[prerequisite[0]]++; // Build inDegree array
-            }
-
-            // Enqueue nodes with zero inDegree
-            Queue<Integer> zeroInDegreeQueue = new ArrayDeque<>();
-            for (int i = 0; i < inDegrees.length; ++i) {
-                if (inDegrees[i] == 0) {
-                    zeroInDegreeQueue.offer(i);
-                }
-            }
-
-            // BFS
-            List<Integer> order = new ArrayList<>();
-            while (!zeroInDegreeQueue.isEmpty()) {
-                int curr = zeroInDegreeQueue.poll();
-                order.add(curr);
-                if (adjacentList[curr] != null) {
-                    for (int next : adjacentList[curr]) {
-                        // -1 inDegree for each neighbour of current node
-                        // Enqueue if inDegree of the neighbour is zero
-                        if (--inDegrees[next] == 0) {
-                            zeroInDegreeQueue.offer(next);
-                        }
-                    }
-                }
-            }
-            return order.size() == numCourses;
-        }
-    }
 }
