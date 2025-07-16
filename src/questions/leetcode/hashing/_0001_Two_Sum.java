0b651c0eb5383419029ce1262e5d7789132d6f95
heyx8826 <155028525@qq.com>
2025-07-15 20:13:40 -0700

    _0001_Two_Sum
    Signed-off-by: heyx8826 <155028525@qq.com>

--- src/questions/leetcode/hashing/_0001_Two_Sum.java
+++ src/questions/leetcode/hashing/_0001_Two_Sum.java
@@ -1,4 +1,4 @@
-package questions.leetcode;
+package questions.leetcode.hashing;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,9 +16,11 @@ Example 1:
     Input: nums = [2,7,11,15], target = 9
     Output: [0,1]
     Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 Example 2:
     Input: nums = [3,2,4], target = 6
     Output: [1,2]
+
 Example 3:
     Input: nums = [3,3], target = 6
     Output: [0,1]
@@ -32,7 +34,7 @@ Constraints:
 
 
 Follow-up:
-    Can you come up with an algorithm that is less than O(n2) time complexity?
+    Can you come up with an algorithm that is less than O(n^2) time complexity?
 
 */
 public class _0001_Two_Sum {
